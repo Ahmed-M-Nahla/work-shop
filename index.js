d6fd055d95ea26cf6f665b1f625435b87d9ddb4d
Ahmed-M-Nahla <Ahmedmohammed121997@gmail.com>
2024-02-17 20:35:33 +0200

    finish project

--- index.js
+++ index.js
@@ -40,3 +40,40 @@ $(".carouse1").flipster({
 
   onItemSwitch: false,
 });
+
+// the end the years date to countdown to 
+
+let countDownDate = new Date("Dec 31 2023 23:59:59").getTime();
+  // console.log(countDownDate);
+
+let counter = setInterval(() => {
+  // get date now
+  let dateNow = new Date().getTime();
+
+  // find the date difference between now and countdown date
+  let dateDiff = countDownDate - dateNow;
+
+  // get time units
+  let days = Math.floor(dateDiff / (1000 * 60 * 60 * 24));
+
+  let hours = Math.floor((dateDiff % (1000 * 60 * 60 * 24)) /(1000 * 60 * 60));
+
+  let minutes = Math.floor((dateDiff % (1000 * 60 * 60)) / (1000 * 60));
+
+  let seconds = Math.floor((dateDiff % (1000 * 60)) /1000 );
+
+  document.getElementById("days").innerHTML = days;
+
+  document.getElementById("hours").innerHTML = hours;
+
+  document.getElementById("minutes").innerHTML = minutes;
+
+  document.getElementById("seconds").innerHTML = seconds < 10 ? `0${seconds}`: seconds;
+
+  // console.log(dateDiff,days,hours,minutes,seconds);
+
+  if (dateDiff < 0) {
+    clearInterval(counter);
+  }
+}, 1000);
+
